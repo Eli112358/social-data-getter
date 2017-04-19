b7a8f6142c10ee33f0a55dc74323ae77858fee13
Eli112358 <Eli112358.SARP@hotmail.com>
2017-04-18 20:50:44 -0400

    Moved callback code to own function
    Added getQueryString.js to index.html

--- getSocialData.js
+++ getSocialData.js
@@ -2,8 +2,10 @@
 // If you did not recieve the included LICENSE file, please goto https://github.com/Eli112358/social-data-getter-js/blob/master/LICENSE.
 
 var resultObj;
-var login = (provider, callback) => {
-	OAuth.redirect(provider, '/social-data-getter');
+var login = (provider) => {
+	OAuth.redirect(provider, '/social-data-getter?provider='+provider);
+}
+var callback = (provider, callback) => {
 	OAuth.callback(provider, {cache: true}).done((p) => {
 		resultObj = p;
 		callback.run(p);
@@ -20,4 +22,7 @@ var getData = (resource, params, callback) => {
 };
 var initialize = () => {
 	OAuth.initialize('PlcWON5gLeLsM92yfNnUQ7_IsXQ');
+	var provider = getURLParameter('provider');
+	var logIt = (p) => {console.log(p)};
+	if(provider != '') callback(provider, {run: logIt, error: logIt});
 };
